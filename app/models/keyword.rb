21759389ace76092ba19a08a13b2ffd4960912a6
Al Sunseri <alsunseri@gmail.com>
2018-08-16 00:11:29 -0500

    add fake scheduler . add grab_all_twits

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -8,7 +8,7 @@ class Keyword < ApplicationRecord
 			config.access_token_secret = "P5WccAWp21e6be2L3RtaeLYHeW5CBtCeFTV1LBKLoYDo7"
 		end
 
-		client.search(self.word,  :count => 10, :result_type =>  "recent").take(10).collect do |tweet|
+		client.search(self.word,  :count => 100, :result_type =>  "recent").take(100).collect do |tweet|
 			new_tweet = Tweet.new
 			new_tweet.tweet_id = tweet.id.to_s
 			new_tweet.tweet_created_at = tweet.created_at	
@@ -16,12 +16,17 @@ class Keyword < ApplicationRecord
 			new_tweet.user_uid = tweet.user.id
 			new_tweet.user_name = tweet.user.name
 			new_tweet.user_screen_name = tweet.user.screen_name
-			new_tweet.user_image_url = tweet.user.profile_image_url
+			new_tweet.user_image_url = tweet.user.profile_image_url.to_s
 
 			new_tweet.keyword=self
 			new_tweet.save
 		end
 	
+	end
+	def self.grab_all_twits
+		Keyword.all.each do |keyword|
+			keyword.grab_twitts
+		end
 	end
 
 end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,6 @@
+#desc "This task would be called by the Heroku scheduler add on "
+task: :grab_twitts => :environment do
+	puts "Grabbing tweets for all keywords in feed"
+	Keyword.grab_all_twitts
+	puts "Done with scheduled Keyword.grab_all_twitts "
+end
